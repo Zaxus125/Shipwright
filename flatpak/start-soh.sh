202c9f84a384e141a6d55b7d740df4c90df78a65
Zaxus <zaxus125@proton.me>
2022-07-25 17:34:49 -0500

    add a flatpak build manifest that might even work

---
+++ flatpak/start-soh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -f oot.otr ]; then
+	soh.elf
+else
+	# TODO: Use the exporter here probably.
+	# TODO: Probably just use the same script as the appimage
+	echo "Place oot.otr in $HOME/.var/app/ID/"
+fi
